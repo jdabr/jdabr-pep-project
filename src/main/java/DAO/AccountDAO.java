1c8d328ba947dd66a86bc985c171b2da2aa3798b
Joseph Dabreu <joeydabreu@gmail.com>
2023-02-12 14:37:04 +0000

    Changed logic for login statement in AccountDAO

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -31,26 +31,30 @@ public class AccountDAO {
         return null;
     }
 
-    public Account loginAccount(Account account){
-        Connection connection = ConnectionUtil.getConnection();
-        try {
-            String sql = "INSERT INTO account (username, password) VALUES (?,?)" ;
-            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+    public Account loginAccount(String username, String password)
+        {
+            Connection connection = ConnectionUtil.getConnection();
+           try {
+            String sql = "SELECT * FROM account WHERE username = ? AND password = ? "; 
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, username); 
+            preparedStatement.setString(2, password); 
+            ResultSet rs = preparedStatement.executeQuery(); 
 
-            preparedStatement.setString(1, account.getUsername());
-            preparedStatement.setString(2, account.getPassword());
-            preparedStatement.executeUpdate();
-
-            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
-            if(pkeyResultSet.next()){
-                int generated_account_id = (int) pkeyResultSet.getLong(1);
-                return new Account(generated_account_id, account.getUsername(), account.getPassword());
+            while(rs.next())
+            {
+                Account account = new Account(rs.getInt("account_id"),
+                rs.getString("username"),
+                rs.getString("password"));
+                return account;
             }
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
+            }catch(SQLException e)
+           {
+            System.out.println(e.getMessage()); 
+           } 
+
+           return null; 
         }
-        return account;
-    }
 
 
 
@@ -58,7 +62,6 @@ public class AccountDAO {
         Connection connection = ConnectionUtil.getConnection();
         List<Account> accounts = new ArrayList<>();
         try {
-            //Write SQL logic here
             String sql = "SELECT * FROM account";
 
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
