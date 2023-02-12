86c0b75b2c5b3e1986bd77f46301fc3c55fbb7f4
Joseph Dabreu <joeydabreu@gmail.com>
2023-02-12 15:48:39 +0000

    Added service logic for MessageDAO functions

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -2,21 +2,69 @@ package Service;
 
 import Model.Message;
 import DAO.MessageDAO;
+
 import java.util.List;
 
 public class MessageService {
     MessageDAO messageDAO;
 
-    public MessageService(){
+    public MessageService()
+    {
         messageDAO = new MessageDAO();
     }
 
-    public MessageService(MessageDAO messageDAO){
+    public MessageService(MessageDAO messageDAO)
+    {
         this.messageDAO = messageDAO;
     }
 
-    public List<Message> getAllMessages(){
+    //logic under here:
+    public List<Message> getAllMessages()
+    {
         return messageDAO.getAllMessages();
+    }
+
+    // public Message addMessageInfo(Message message)
+    // {
+    //     return messageDAO.addMessage(message);
+    // }
+
+    public Message insertMessageInfo(Message message)
+    {
+        return messageDAO.insertMessage(message);
+    }
+
+    //??
+    public Message getMessageAfterPosting(int id)
+    {
+        return messageDAO.getMessage(id);
+    }
+
+    //Delete works, but when the API calls the message it's not deleted
+    public Message deleteMessage(int id, Message message)
+    {
+        //Message test = messageDAO.getMessage(id);
+        // test.setMessage_id(message.getMessage_id());
+        message.setMessage_id(id);
+        messageDAO.deletedMessage(id, message);
+        //messageDAO.deletedMessage(id, message);
+        return message;
+    }
+
+    //Update works, but gets 500 error when called for a non existing message
+    public Message patchMessageText(int id, Message message)
+    {
+        message.setMessage_text(message.getMessage_text());
+        message.setMessage_id(id);
+        message.setPosted_by(id);
+        message.setTime_posted_epoch(messageDAO.getMessage(id).getTime_posted_epoch());
+        messageDAO.patchMessage(id, message);
+        return message;
+    }
+
+    public List<Message> getAllMessagesByAccountId()
+    {
+        return messageDAO.getAllMessagesByAccountId();
     }
 
 }
